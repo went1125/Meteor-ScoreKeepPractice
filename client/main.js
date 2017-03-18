a44d8702189cb2200b963eb52b8f70e1bca707fd
wayne <went1125@gmail.com>
2017-03-19 01:04:22 +0800

    CRUD done

--- client/main.js
+++ client/main.js
@@ -9,6 +9,12 @@ const renderPlayers = (playerList) => {
         return (
             <p key={player._id}>
                 {player.name} has {player.score} point(s).
+                <button onClick={() => Players.update({_id: player._id}, {
+                    $inc: {score: 1}
+                })}>+1</button>
+                <button onClick={() => Players.update({_id: player._id}, {
+                    $inc: {score: -1}
+                })}>-1</button>
                 <button onClick={() => Players.remove({_id: player._id})}>X</button>
             </p>
         );
