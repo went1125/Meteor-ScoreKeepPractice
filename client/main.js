a2dab4276d8eb53e1c8fe24e5662f18f5c3ed4d3
wayne <went1125@gmail.com>
2017-03-18 17:24:26 +0800

    add done

---
+++ client/main.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+import {Tracker} from 'meteor/tracker';
+
+const renderPlayers = (playerList) => {
+    return playerList.map((player) => <p key={player._id}>{player.name} has {player.score} point(s).</p>);
+}
+
+const submitHandle = (e) => {
+    e.preventDefault();
+    playerName = e.target.playerName.value;
+    if(playerName) {
+        e.target.playerName.value = '';
+        Players.insert({
+            name: playerName,
+            score: 0
+        });
+    }
+}
+Meteor.startup(() => {
+    Tracker.autorun(() => {
+        let players = Players.find().fetch();
+        let title = 'Score keep'
+        let name = 'Wayne';
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>{renderPlayers(players)}</p>
+                <form onSubmit={submitHandle}>
+                    <input type="text" name="playerName" placeholder="Player name"/>
+                    <button>add Player</button>
+                </form>
+            </div>
+        );
+        ReactDom.render(jsx, document.getElementById('app'));
+    });
+});
